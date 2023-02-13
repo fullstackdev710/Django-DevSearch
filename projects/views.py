91c06f4e0bbd1d3151af70080402f6fa4762b8f3
fullstackdev710 <fullstack710dev@gmail.com>
2023-02-14 02:09:35 +0800

    Initialize Django-DevSearch Project.

---
+++ projects/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+# Create your views here.
+
+projectsList = [
+    {
+        'id': '1',
+        'title': "Ecommerce Website",
+        'description': "Fully functional ecommerce website."
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': "This was a project where I built out my portfolio."
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': "Awesome open source project I am still working on."
+    },
+]
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})
