f3d35fbfb4621f63d984b11a7f7c2d1a8427fe54
fullstackdev710 <fullstack710dev@gmail.com>
2023-02-14 21:35:36 +0800

    Model Forms

--- projects/views.py
+++ projects/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
-
-projectsList = [
-    {
-        'id': '1',
-        'title': "Ecommerce Website",
-        'description': "Fully functional ecommerce website."
-    },
-    {
-        'id': '2',
-        'title': "Portfolio Website",
-        'description': "This was a project where I built out my portfolio."
-    },
-    {
-        'id': '3',
-        'title': "Social Network",
-        'description': "Awesome open source project I am still working on."
-    },
-]
 
 
 def projects(request):
@@ -33,3 +16,9 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
