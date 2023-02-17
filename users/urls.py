85185ef5e2288c778867970778298b08641dfd57
fullstackdev710 <fullstack710dev@gmail.com>
2023-02-18 04:17:27 +0800

    User Login, Logout and Flash Messages

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('', views.profiles, name="profiles"),
     path("user-profile/<str:pk>/", views.profile, name="user-profile"),
 ]
