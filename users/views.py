e7c08db78d6026f3e77a12603532dd9f60c97b17
fullstackdev710 <fullstack710dev@gmail.com>
2023-02-21 23:31:20 +0800

    Search

--- users/views.py
+++ users/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .models import Profile
+from django.db.models import Q
+from .models import Profile, Skill
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .utils import searchProfiles
 
 # Create your views here.
 
@@ -64,8 +66,9 @@ def registerUser(request):
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {"profiles": profiles}
+    profiles, search_query = searchProfiles(request)
+
+    context = {"profiles": profiles, "search_query": search_query}
     return render(request, 'users/profiles.html', context)
 
 
