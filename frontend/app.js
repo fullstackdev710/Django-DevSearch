1762d6a4c4fd83c0e76fb5f9b99207454da082cf
fullstackdev710 <fullstack710dev@gmail.com>
2023-02-25 09:09:08 +0800

    Sending Data from the Frontend

--- frontend/app.js
+++ frontend/app.js
@@ -10,10 +10,37 @@ var getProjects = function () {
 };
 var buildProjects = function (projects) {
     var projectsWrapper = document.getElementById("projects-wrapper");
+    projectsWrapper.innerHTML = '';
     for (var i = 0; projects.length > i; i++) {
         var project = projects[i];
-        var projectCard = "\n         <div class=\"project--card\">\n            <img src=\"http://127.0.0.1:8000/".concat(project.featured_image, "\" />\n            <div>\n               <div class=\"card--header\">\n                  <h3>").concat(project.title, "</h3>\n                  <strong class=\"vote--option\">&#43;</strong>\n                  <strong class=\"vote--option\">&#8722;</strong>\n               </div>\n               <i>").concat(project.vote_ratio, "% Positive feedback</i>\n               <p>").concat(project.description.substring(0, 150), "</p>\n            </div>\n         </div>\n      ");
+        var projectCard = "\n         <div class=\"project--card\">\n            <img src=\"http://127.0.0.1:8000/".concat(project.featured_image, "\" />\n            <div>\n               <div class=\"card--header\">\n                  <h3>").concat(project.title, "</h3>\n                  <strong class=\"vote--option\" data-vote=\"up\" data-project=\"").concat(project.id, "\">&#43;</strong>\n                  <strong class=\"vote--option\" data-vote=\"down\" data-project=\"").concat(project.id, "\">&#8722;</strong>\n               </div>\n               <i>").concat(project.vote_ratio, "% Positive feedback</i>\n               <p>").concat(project.description.substring(0, 150), "</p>\n            </div>\n         </div>\n      ");
         projectsWrapper.innerHTML += projectCard;
+    }
+    addVoteEvents();
+    //Add an Listener
+};
+var addVoteEvents = function () {
+    var voteBtns = document.getElementsByClassName('vote--option');
+    for (var i = 0; voteBtns.length > i; i++) {
+        voteBtns[i].addEventListener('click', function (e) {
+            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc3MzcyNzAzLCJpYXQiOjE2NzcyODYzMDMsImp0aSI6ImYwZDJiYzc3OWMwYTQ4NjZhOGQ0ZTI1Yjc4N2NlMjdlIiwidXNlcl9pZCI6MX0.dfFIn0xVr78n050U3_NvuTdh_idR08VNTlfNm3jkYbc";
+            var target = e.target;
+            var vote = target.dataset.vote;
+            var project = target.dataset.project;
+            fetch("http://127.0.0.1:8000/api/project/".concat(project, "/vote/"), {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: "Bearer ".concat(token)
+                },
+                body: JSON.stringify({ 'value': vote })
+            })
+                .then(function (response) { return response.json(); })
+                .then(function (data) {
+                console.log('Success:', data);
+                getProjects();
+            });
+        });
     }
 };
 getProjects();
--- frontend/app.ts
+++ frontend/app.ts
@@ -1,39 +1,67 @@
 let message: string = 'Hello, World!';
 
-let projectUrl='http://127.0.0.1:8000/api/projects/';
+let projectUrl = 'http://127.0.0.1:8000/api/projects/';
 
-let getProjects=()=>{
+let getProjects = () => {
    fetch(projectUrl)
-   .then(response=>response.json())
-   .then(data=>{
-      console.log(data)
-      buildProjects(data);
-   })
+      .then(response => response.json())
+      .then(data => {
+         console.log(data)
+         buildProjects(data);
+      })
 }
 
-let buildProjects=(projects)=>{
-   let projectsWrapper = document.getElementById("projects-wrapper")
+let buildProjects = (projects) => {
+   let projectsWrapper = document.getElementById("projects-wrapper") as HTMLBodyElement;
+   projectsWrapper.innerHTML = ''
+   for (let i = 0; projects.length > i; i++) {
+      let project = projects[i]
 
-   for(let i=0;projects.length>i;i++) {
-      let project =projects[i]
-
-      let projectCard=`
+      let projectCard = `
          <div class="project--card">
             <img src="http://127.0.0.1:8000/${project.featured_image}" />
             <div>
                <div class="card--header">
                   <h3>${project.title}</h3>
-                  <strong class="vote--option">&#43;</strong>
-                  <strong class="vote--option">&#8722;</strong>
+                  <strong class="vote--option" data-vote="up" data-project="${project.id}">&#43;</strong>
+                  <strong class="vote--option" data-vote="down" data-project="${project.id}">&#8722;</strong>
                </div>
                <i>${project.vote_ratio}% Positive feedback</i>
-               <p>${project.description.substring(0,150)}</p>
+               <p>${project.description.substring(0, 150)}</p>
             </div>
          </div>
       `
-      projectsWrapper.innerHTML +=projectCard;
+      projectsWrapper.innerHTML += projectCard;
    }
+   addVoteEvents()
+   //Add an Listener
+}
 
+let addVoteEvents = () => {
+   let voteBtns = document.getElementsByClassName('vote--option')
+
+   for (let i = 0; voteBtns.length > i; i++) {
+      voteBtns[i].addEventListener('click', (e) => {
+         let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc3MzcyNzAzLCJpYXQiOjE2NzcyODYzMDMsImp0aSI6ImYwZDJiYzc3OWMwYTQ4NjZhOGQ0ZTI1Yjc4N2NlMjdlIiwidXNlcl9pZCI6MX0.dfFIn0xVr78n050U3_NvuTdh_idR08VNTlfNm3jkYbc";
+         const target = e.target as HTMLElement;
+         let vote = target.dataset.vote
+         let project = target.dataset.project
+
+         fetch(`http://127.0.0.1:8000/api/project/${project}/vote/`, {
+            method: 'POST', 
+            headers: {
+               'Content-Type': 'application/json',
+               Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({'value': vote})
+         })
+         .then(response => response.json())
+         .then(data => {
+            console.log('Success:', data)
+            getProjects();
+         })
+   })
+}
 }
 
 getProjects();
